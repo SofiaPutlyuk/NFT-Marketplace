ee48d236044db1c1c1b58e760a29baab89be8ca1
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2025-07-24 13:27:21 +0300

    final part of work with banner

--- nft-marketplace/src/components/MainPage/Banner/Banner.jsx
+++ nft-marketplace/src/components/MainPage/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import RocketStart from '../../../logo/RocketStart.svg'
 export const Banner = () => {
     const [info, setInfo] = useState([])
@@ -17,26 +17,44 @@ export const Banner = () => {
         fetchUser()
     }, [])
     return (
-        <div className="banner">
-            <p className="banner-text">Discover digital art & Collect NFTs</p>
-            <p className="banner-description">NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
-            <div className="banner-card">
-                <img src={info.avatar} alt={info.description} className="card-image" />
-                <p className="card-description">{info.description}</p>
-                <div className="user-container">
-                    <img src={info.userAvatar} alt="user" className="user-image" />
-                    <p className="user-text">{info.user}</p>
-                </div>
-            </div>
-            <button className="banner-button">
-                <img src={RocketStart} alt="logo-start" />
-                <p className="text-start">Get Started</p>
-            </button>
-            <div className="banner-rating">
-                <p className="rating-text"><span className="rating-span">240k+</span>Total Sale</p>
-                <p className="rating-text"><span className="rating-span">100k+</span>Auctions</p>
-                <p className="rating-text"><span className="rating-span">100k+</span>Artists</p>
-            </div>
-        </div>
+        <Fragment>
+      <div className="banner">
+  <div className="banner-left">
+    <p className="banner-text">Discover digital art & Collect NFTs</p>
+    <p className="banner-description">
+      NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.
+    </p>
+    <div className="hide-card">
+      <div className="banner-card">
+    <img src={info.avatar} alt={info.description} className="card-image" />
+    <p className="card-description">{info.description}</p>
+    <div className="user-container">
+      <img src={info.userAvatar} alt="user" className="user-image" />
+      <p className="user-text">{info.user}</p>
+    </div>
+  </div>
+  </div>
+    <button className="banner-button">
+      <img src={RocketStart} alt="logo-start" />
+      <p className="text-start">Get Started</p>
+    </button>
+    <div className="banner-rating">
+      <p className="rating-text"><span className="rating-span">240k+</span>Total Sale</p>
+      <p className="rating-text"><span className="rating-span">100k+</span>Auctions</p>
+      <p className="rating-text"><span className="rating-span">240k+</span>Artists</p>
+    </div>
+  </div>
+
+  <div className="banner-card hide-mobile">
+    <img src={info.avatar} alt={info.description} className="card-image" />
+    <p className="card-description">{info.description}</p>
+    <div className="user-container">
+      <img src={info.userAvatar} alt="user" className="user-image" />
+      <p className="user-text">{info.user}</p>
+    </div>
+  </div>
+</div>
+
+            </Fragment>
     )
 }
